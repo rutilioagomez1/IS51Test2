34624d91d3fbe2c3abb53946d9f11aee0080914f
unknown <rutilioagomez1@mail.fresnostate.edu>
2021-04-29 23:16:14 -0700

    finish pseudocode

--- IS51Test2.py
+++ IS51Test2.py
@@ -17,3 +17,34 @@ end
 """
 
 
+"""
+# main():
+    enter code Testscores = input("Enter 24 student grades on a final exam seperated by commas:")
+    listScores = scores.split(",")`enter code here`
+    determine_grade(listScores)
+    calc_average(listScores)
+
+# determine_grade(grades):
+    for num in grades:
+        if int(num) >= 90 and int(num) <= 100:
+            print("A")
+        elif int(num) >=80 and int(num) <= 89:
+            print("B")
+        elif int(num) >=70 and int(num) <= 79:
+            print("C")
+        elif int(num) >=60 and int(num) <= 69:
+            print("D")
+        else:
+            print("F")
+
+
+# calc_average(grades):
+    total = 
+    for num in grades:
+        total += int(num)
+    average = total / 24
+    print(average)
+
+
+# show_letters(values):
+main()
